8bbaa1e929b732c84270f13eb61b98b725d90934
sagaway1 <yue.sagawa@mail.utoronto.ca>
2020-08-23 03:47:05 -0400

    Button testing

---
+++ Go!/Go!/Go!/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Go!
+//
+//  Created by 蔡蔡 on 2020-08-23.
+//  Copyright © 2020 蔡蔡. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/)
+            }
+            Text("Hello, World!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
