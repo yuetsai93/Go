694c3f22a53222bf106bae60ede5ba8ac7ae01e0
sagaway1 <yue.sagawa@mail.utoronto.ca>
2020-08-26 03:07:27 -0400

    Main page UI first draft

--- Go!/Go!/Go!/ContentView.swift
+++ Go!/Go!/Go!/ContentView.swift
@@ -10,11 +10,66 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                Text(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/)
+        VStack {
+            GeometryReader { g in
+//                    Image("goIcon").resizable().scaledToFit().padding(10)
+               
+                
+                Text("Go!")
+                        .font(.largeTitle).fontWeight(.black).foregroundColor(MyColors.greengray)
+                    .frame(width: g.size.width/6, height: g.size.height/10)
+                    .offset(x: g.size.width/2, y: 0)
+                
+
+                
+//                Spacer()
+//                HStack {
+                     Button(action: {
+                        print("Menu pressed!")
+                        }) {
+                            Image("menuIcon").resizable().scaledToFit().padding(10)
+                                .frame(width: g.size.width/6, height: g.size.height/10)
+    //                            .foregroundColor(Color.white)
+                    }
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(MyColors.greengray, lineWidth: g.size.width/150))
+//                    .padding(.leading)
+                     .offset(x: g.size.width/1000, y: g.size.height - 2.125 * (g.size.height/10))
+//                    Spacer()
+//                }
+                
+                // Bottom row
+//                HStack {
+                    // Settings button
+                    Button(action: {
+                        print("Settiings pressed!")
+                        }) {
+                            Image("settingsIcon").resizable().scaledToFit().padding()
+                                .frame(width: g.size.width/6, height: g.size.height/10)
+                    }
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(MyColors.greengray, lineWidth: g.size.width/150))
+//                    .background(Capsule().stroke(MyColors.greengray, lineWidth: 3))
+//                    .padding(.leading)
+                    .offset(x: g.size.width/1000, y: g.size.height - g.size.height/10)
+
+
+                    // AddPlan button
+//                    Spacer()
+                    Button(action: {
+                        print("New Plan!")
+                        }) {
+                            Image("newIcon").resizable().scaledToFit().padding(30)
+                                .frame(width: g.size.width/6, height: g.size.height/10)
+                                .foregroundColor(Color.white)
+                                .background(MyColors.greengray)
+                            .clipShape(Circle())
+                    }
+//                    .padding(.trailing)
+                    .offset(x: g.size.width - g.size.width/6, y: g.size.height - g.size.height/10)
+
+//                }
             }
-            Text("Hello, World!")
         }
     }
 }
---
+++ Go!/Go!/Go!/MyColors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  Go!
+//
+//  Created by 蔡蔡 on 2020-08-25.
+//  Copyright © 2020 蔡蔡. All rights reserved.
+//
+
+import SwiftUI
+
+struct MyColors {
+    static let greengray = Color("GreenGray")
+}
